a983ac6fc8ba2dc39a4e142eee09571be3d2f0f7
jlieow <jerome.lieow@mile.cloud>
2022-09-26 16:34:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Everyone!\n';
+  const msg = 'Hello Private!\n';
   res.end(msg);
 });
 
